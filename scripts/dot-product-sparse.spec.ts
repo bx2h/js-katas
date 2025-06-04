442fef974ca9f41f0bf9e2144b686647f0fbf9ac
bx2h <bx2h.git@gmail.com>
2025-06-05 00:53:10 +0100

    enhance: added alternative to hash map solution to dot product sparse

--- scripts/dot-product-sparse.spec.ts
+++ scripts/dot-product-sparse.spec.ts
@@ -1,49 +1,72 @@
 import { describe, it, expect } from 'vitest'
-import { SparseVector } from './dot-product-sparse'
+import { SparseVector, AltSparseVector } from './dot-product-sparse'
 
 describe('Dot Product sparse vector', () => {
     it('Creates dot product of two single vectors', () => {
-        const v1 = new SparseVector([2])
-        const v2 = new SparseVector([3])
-        const ans = v1.dotProduct(v2)
+        let ans = new SparseVector([2]).dotProduct(new SparseVector([3]))
+        expect(ans).toEqual(6)
+
+        ans = new AltSparseVector([2]).dotProduct(new AltSparseVector([3]))
         expect(ans).toEqual(6)
     })
 
     it('Creates dot product of two vectors', () => {
-        const v1 = new SparseVector([1, 2, 3, 4])
-        const v2 = new SparseVector([1, 2, 3, 5])
-        const ans = v1.dotProduct(v2)
+        let ans = new SparseVector([1, 2, 3, 4]).dotProduct(
+            new SparseVector([1, 2, 3, 5])
+        )
+        expect(ans).toEqual(1 + 4 + 9 + 20)
+
+        ans = new AltSparseVector([1, 2, 3, 4]).dotProduct(
+            new AltSparseVector([1, 2, 3, 5])
+        )
         expect(ans).toEqual(1 + 4 + 9 + 20)
     })
 
     it('Creates dot product of two sparse vectors', () => {
-        let v1 = new SparseVector([1, 0, 0, 2, 3])
-        let v2 = new SparseVector([0, 3, 0, 4, 0])
-        let ans = v1.dotProduct(v2)
+        let ans = new SparseVector([1, 0, 0, 2, 3]).dotProduct(
+            new SparseVector([0, 3, 0, 4, 0])
+        )
         expect(ans).toEqual(8)
 
-        v1 = new SparseVector([0, 1, 0, 0, 0])
-        v2 = new SparseVector([0, 0, 0, 0, 2])
-        ans = v1.dotProduct(v2)
+        ans = new SparseVector([0, 1, 0, 0, 0]).dotProduct(
+            new SparseVector([0, 0, 0, 0, 2])
+        )
         expect(ans).toEqual(0)
 
-        v1 = new SparseVector([0, 1, 0, 0, 2, 0, 0])
-        v2 = new SparseVector([1, 0, 0, 0, 3, 0, 4])
-        ans = v1.dotProduct(v2)
+        ans = new SparseVector([0, 1, 0, 0, 2, 0, 0]).dotProduct(
+            new SparseVector([1, 0, 0, 0, 3, 0, 4])
+        )
+        expect(ans).toEqual(6)
+
+        ans = new AltSparseVector([1, 0, 0, 2, 3]).dotProduct(
+            new AltSparseVector([0, 3, 0, 4, 0])
+        )
+        expect(ans).toEqual(8)
+
+        ans = new AltSparseVector([0, 1, 0, 0, 0]).dotProduct(
+            new AltSparseVector([0, 0, 0, 0, 2])
+        )
+        expect(ans).toEqual(0)
+
+        ans = new AltSparseVector([0, 1, 0, 0, 2, 0, 0]).dotProduct(
+            new AltSparseVector([1, 0, 0, 0, 3, 0, 4])
+        )
         expect(ans).toEqual(6)
     })
 
     it('Creates dot product of one empty vector', () => {
-        const v1 = new SparseVector([4])
-        const v2 = new SparseVector([])
-        const ans = v1.dotProduct(v2)
+        let ans = new SparseVector([4]).dotProduct(new SparseVector([]))
+        expect(ans).toEqual(0)
+
+        ans = new AltSparseVector([4]).dotProduct(new AltSparseVector([]))
         expect(ans).toEqual(0)
     })
 
     it('Creates dot product of two empty vectors', () => {
-        const v1 = new SparseVector([])
-        const v2 = new SparseVector([])
-        const ans = v1.dotProduct(v2)
+        let ans = new SparseVector([]).dotProduct(new SparseVector([]))
+        expect(ans).toEqual(0)
+
+        ans = new AltSparseVector([]).dotProduct(new AltSparseVector([]))
         expect(ans).toEqual(0)
     })
 })
--- scripts/dot-product-sparse.ts
+++ scripts/dot-product-sparse.ts
@@ -1,3 +1,16 @@
+// Time: O(n) worst case
+// O(n) - for constructor
+// O(L) where L is number of non-zero elements
+
+// Space: O(L) worst case
+// O(L) for creating the hash map
+// O(1) for calculating the dot product
+
+// Due to the potential complexity and storage of the Hash function in a map
+// We could instead store nums as [key, value] pairs, excluding 0 values
+// Then during dotProduct, we use two indexes to move through each SparseVector independelty.
+// See AltSparseVector
+
 interface SparseVectorInstance {
     nums: Map<number, number>
     dotProduct(vec: SparseVectorInstance): number
@@ -31,7 +44,47 @@ SparseVector.prototype.dotProduct = function (vec: SparseVectorInstance) {
     return sum
 }
 
-export { SparseVector }
+interface AltSparseVectorInstance {
+    nums: number[][]
+    dotProduct(vec: AltSparseVectorInstance): number
+}
+
+interface AltSparseVectorConstructor {
+    new (nums: number[]): AltSparseVectorInstance
+    prototype: AltSparseVectorInstance
+}
+
+const AltSparseVector = function (nums: number[]) {
+    this.nums = []
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            this.nums.push([i, nums[i]])
+        }
+    }
+} as unknown as AltSparseVectorConstructor
+
+AltSparseVector.prototype.dotProduct = function (vec: AltSparseVectorInstance) {
+    let sum = 0
+    let q = 0
+    let p = 0
+
+    while (q < this.nums.length && p < vec.nums.length) {
+        if (this.nums[q][0] === vec.nums[p][0]) {
+            sum += this.nums[q][1] * vec.nums[p][1]
+            p++
+            q++
+        } else if (this.nums[q][0] < vec.nums[p][0]) {
+            q++
+        } else {
+            p++
+        }
+    }
+
+    return sum
+}
+
+export { SparseVector, AltSparseVector }
 
 // Your SparseVector object will be instantiated and called as such:
 // let v1 = new SparseVector(nums1);
